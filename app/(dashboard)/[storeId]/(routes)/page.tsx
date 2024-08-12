af8070a96fb986748bb175cdcea363c18290653e
abdalla ALsaeed <Abdalla>
2024-08-12 16:34:17 +0200

    add card2

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { db } from "@/lib/db";
 import { formater } from "@/lib/utils";
-import { DollarSign } from "lucide-react";
+import { CreditCard, DollarSign, Package } from "lucide-react";
 import React from "react";
 
 interface DashboardPageProps {
@@ -22,7 +22,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <MainHeader lebel="Dashboard" description="OverView of your store" />
+        <MainHeader lebel="Dashboard" description="Overview of your store" />
         <Separator />
         <div className="grid gap-4 grid-cols-4 ">
           <Card>
@@ -38,10 +38,8 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
           </Card>
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-4">
-              <CardTitle className="text-sm font-medium">
-                Total Revenue
-              </CardTitle>
-              <DollarSign className="w-4 h-4 text-muted-foreground" />
+              <CardTitle className="text-sm font-medium">Sales</CardTitle>
+              <CreditCard className="w-4 h-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
               <div className="text-2xl font-bold">{formater.format(100)}</div>
@@ -50,23 +48,12 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-4">
               <CardTitle className="text-sm font-medium">
-                Total Revenue
+                Product In Stock
               </CardTitle>
-              <DollarSign className="w-4 h-4 text-muted-foreground" />
+              <Package className="w-4 h-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">{formater.format(100)}</div>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-4">
-              <CardTitle className="text-sm font-medium">
-                Total Revenue
-              </CardTitle>
-              <DollarSign className="w-4 h-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">{formater.format(100)}</div>
+              <div className="text-2xl font-bold">{12}</div>
             </CardContent>
           </Card>
         </div>
