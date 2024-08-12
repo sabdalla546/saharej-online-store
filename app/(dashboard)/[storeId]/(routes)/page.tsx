0bfdeca8e82d45214a0622e6beda0ca4e3806193
abdalla ALsaeed <Abdalla>
2024-08-12 16:05:07 +0200

    create dashboared

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,20 @@
-const DashboardPage = () => {
-  return <div className="p-5">store dashboard</div>;
+import { db } from "@/lib/db";
+import React from "react";
+
+interface DashboardPageProps {
+  params: {
+    storeId: string;
+  };
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await db.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+
+  return <div className="p-5">store name {store?.name}</div>;
 };
 
 export default DashboardPage;
