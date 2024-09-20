78d10a9e1f6ce6378e2ee636f6c6e3454eb44f4c
abdalla ALsaeed <Abdalla>
2024-09-20 11:40:34 +0200

    fix missing secret

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -13,98 +13,71 @@ import {
   Tag,
   UserRoundCog,
 } from "lucide-react";
-import { currentUser } from "@/lib/auth";
 
-export const MainNav = async ({
+export const MainNav = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) => {
   const pathName = usePathname();
   const params = useParams();
-  const user = await currentUser();
-  let routes;
-  if (user?.role === "SUPERADMIN") {
-    routes = [
-      {
-        href: `/`,
-        label: "Dashboard",
-        icon: <LayoutDashboard />,
-        active: pathName === `/`,
-      },
 
-      {
-        href: `/category`,
-        label: "category",
-        icon: <ImagePlus />,
-        active: pathName === `/category`,
-      },
-      {
-        href: `/supcategory`,
-        label: "sup category",
-        icon: <ImagePlus />,
-        active: pathName === `/supcategory`,
-      },
-    ];
-  } else {
-    routes = [
-      {
-        href: `/${params.storeId}`,
-        label: "Dashboard",
-        icon: <LayoutDashboard />,
-        active: pathName === `/${params.storeId}`,
-      },
+  const routes = [
+    {
+      href: `/${params.storeId}`,
+      label: "Dashboard",
+      icon: <LayoutDashboard />,
+      active: pathName === `/${params.storeId}`,
+    },
 
-      {
-        href: `/${params.storeId}/billboards`,
-        label: "Billboards",
-        icon: <ImagePlus />,
-        active: pathName === `/${params.storeId}/billboards`,
-      },
-      {
-        href: `/${params.storeId}/categories`,
-        label: "Categories",
-        icon: <Layers3 />,
-        active: pathName === `/${params.storeId}/categories`,
-      },
-      {
-        href: `/${params.storeId}/sizes`,
-        label: "Sizes",
-        icon: <PencilRuler />,
-        active: pathName === `/${params.storeId}/sizes`,
-      },
-      {
-        href: `/${params.storeId}/colors`,
-        label: "Colors",
-        icon: <Palette />,
-        active: pathName === `/${params.storeId}/colors`,
-      },
-      {
-        href: `/${params.storeId}/products`,
-        icon: <Tag />,
-        label: "Products",
-        active: pathName === `/${params.storeId}/products`,
-      },
-      {
-        href: `/${params.storeId}/orders`,
-        icon: <ShoppingBag />,
-        label: "Orders",
-        active: pathName === `/${params.storeId}/orders`,
-      },
-      {
-        href: `/${params.storeId}/profile`,
-        icon: <UserRoundCog />,
-        label: "Profile",
-        active: pathName === `/${params.storeId}/profile`,
-      },
-      {
-        href: `/${params.storeId}/settings`,
-        label: "Settings",
-        icon: <Settings />,
-        active: pathName === `/${params.storeId}/settings`,
-      },
-    ];
-  }
-
+    {
+      href: `/${params.storeId}/billboards`,
+      label: "Billboards",
+      icon: <ImagePlus />,
+      active: pathName === `/${params.storeId}/billboards`,
+    },
+    {
+      href: `/${params.storeId}/categories`,
+      label: "Categories",
+      icon: <Layers3 />,
+      active: pathName === `/${params.storeId}/categories`,
+    },
+    {
+      href: `/${params.storeId}/sizes`,
+      label: "Sizes",
+      icon: <PencilRuler />,
+      active: pathName === `/${params.storeId}/sizes`,
+    },
+    {
+      href: `/${params.storeId}/colors`,
+      label: "Colors",
+      icon: <Palette />,
+      active: pathName === `/${params.storeId}/colors`,
+    },
+    {
+      href: `/${params.storeId}/products`,
+      icon: <Tag />,
+      label: "Products",
+      active: pathName === `/${params.storeId}/products`,
+    },
+    {
+      href: `/${params.storeId}/orders`,
+      icon: <ShoppingBag />,
+      label: "Orders",
+      active: pathName === `/${params.storeId}/orders`,
+    },
+    {
+      href: `/${params.storeId}/profile`,
+      icon: <UserRoundCog />,
+      label: "Profile",
+      active: pathName === `/${params.storeId}/profile`,
+    },
+    {
+      href: `/${params.storeId}/settings`,
+      label: "Settings",
+      icon: <Settings />,
+      active: pathName === `/${params.storeId}/settings`,
+    },
+  ];
   return (
     <nav className={cn("flex flex-col items-start  gap-8 ", className)}>
       {routes.map((route) => (
