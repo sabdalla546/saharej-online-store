00f1147e5f8586c032cd49304e01bad98142fdd8
abdalla ALsaeed <Abdalla>
2024-09-24 00:09:03 +0200

    fix add featured3

--- app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/_component/category-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/_component/category-form.tsx
@@ -58,7 +58,7 @@ export const CategoryForm = ({
     defaultValues: {
       name: initialData?.name,
       billboardId: initialData?.billboardId,
-      isFeatured: initialData?.isFeatured,
+      isFeatured: initialData?.isFeatured || null || undefined,
     },
   });
   async function onSubmit(values: z.infer<typeof CategorySchema>) {
