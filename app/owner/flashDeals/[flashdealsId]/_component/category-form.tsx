cc75195792420bea11c8199f590fbe7ce5753d5d
abdalla ALsaeed <Abdalla>
2024-09-25 15:16:31 +0200

    fix  flash deals5

--- app/owner/flashDeals/[flashdealsId]/_component/category-form.tsx
+++ app/owner/flashDeals/[flashdealsId]/_component/category-form.tsx
@@ -182,6 +182,9 @@ export const CategoryForm = ({ initialData }: FlasDealsFormProps) => {
                         mode="single"
                         selected={field.value}
                         onSelect={field.onChange}
+                        disabled={(date) =>
+                          date > new Date() || date < new Date("1900-01-01")
+                        }
                         initialFocus
                       />
                     </PopoverContent>
