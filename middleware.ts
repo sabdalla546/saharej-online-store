4f788e88023006195fc022b099a597b7ddf8ada1
abdalla ALsaeed <sabdalla546@gmail.com>
2024-10-22 01:00:36 +0200

    fix login5

--- middleware.ts
+++ middleware.ts
@@ -21,8 +21,7 @@ export default auth((req) => {
 
   console.log(isLoggedIn);
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiRoutePrefix);
-  const isPuplicRoutes =
-    publicRoutes.includes(nextUrl.pathname) || "/api/:path*";
+  const isPuplicRoutes = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoutes = authRoutes.includes(nextUrl.pathname);
   if (isApiAuthRoute) {
     return;
